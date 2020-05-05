38d90e8de0c71f90d46668aee37c9f29ed2755cc
shinsaito <s.shin212.mb13@gmail.com>
2020-05-05 23:43:07 +0900

    Update download.py  0505.2020

--- download.py
+++ download.py
@@ -8,43 +8,43 @@ key = ""
 secret = ""
 wait_time = 1
 
-#save directory
-#make new directory for image save
+###
 savedir_root = "./" + "images"
 image_name = []
 
 for i in range(len(sys.argv) - 1):
  
+    #make new directory for image save
     savedir_path = os.path.join(savedir_root, sys.argv[i + 1])
     os.mkdir(savedir_path)
     image_name.append(sys.argv[i + 1])
 
-'''
-#get flickr data as json
-flickr = FlickrAPI(key, secret, format='parsed-json')
-result = flickr.photos.search(
-    text = searchname,
-    per_page = 400,
-    media = 'photos',
-    sort = 'relevance',
-    safe_search = 1,
-    extras = 'url_q, licence'
-)
 
-photos = result['photos']
-# check flickrapi json
-# pprint(photos)
+    #get flickr data as json
+    flickr = FlickrAPI(key, secret, format='parsed-json')
+    result = flickr.photos.search(
+        text = image_name[i],
+        per_page = 400,
+        media = 'photos',
+        sort = 'relevance',
+        safe_search = 1,
+        extras = 'url_q, licence'
+    )
 
-#get photo file 
-for i, photo in enumerate(photos['photo']):
-    url_q = photo['url_q']
-    filepath = savedir + '/' + photo['id'] + '.jpg'
+    photos = result['photos']
 
-    # check file exist or not
-    if os.path.exists(filepath): continue
-    # download photo file
-    urlretrieve(url_q, filepath)
-    # 1se sleep for server stress
-    time.sleep(wait_time)
-'''
+    # check flickrapi json
+    # pprint(photos)
+
+    #get photo file 
+    for j, photo in enumerate(photos['photo']):
+        url_q = photo['url_q']
+        filepath = savedir_path + '/' + photo['id'] + '.jpg'
+
+        # check file exist or not
+        if os.path.exists(filepath): continue
+        # download photo file
+        urlretrieve(url_q, filepath)
+        # 1se sleep for server stress
+        time.sleep(wait_time)
 
